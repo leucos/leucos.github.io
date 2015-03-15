370c0e80a3964f0c7dc9ea79fffda4bc7ff91941
Michel Blanc <mb@mbnet.fr>
2015-03-15 16:11:41 +0100

    [wip] Changes Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,8 +1,8 @@
 require "rubygems"
 require "bundler/setup"
-#require "stringex"
+require "stringex"
 require 'rake'
-#require 'rdoc'
+require 'rdoc'
 require 'date'
 require 'yaml'
 require 'tmpdir'
@@ -44,8 +44,97 @@ desc "Generate and publish blog to gh-pages"
 task :publish do
   Dir.mktmpdir do |tmp|
     system "jgd"
+    # system "mv _site/* #{tmp}"
+    # system "git checkout -B gh-pages"
+    # system "rm -rf *"
+    # system "mv #{tmp}/* ."
+    # message = "Site updated at #{Time.now.utc}"
+    # system "git add ."
+    # system "git commit -am #{message.shellescape}"
+    # system "git push origin gh-pages --force"
+    # system "git checkout master"
+    # system "echo yolo"
   end
 end
 task :deploy => :publish
 task :default => :publish
 
+#############################
+# Create a new Post or Page #
+#############################
+
+# usage rake new_post
+desc "Create a new post in #{posts_dir}"
+task :new_post, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin("Enter a title for your post: ")
+  end
+  filename = "#{posts_dir}/#{Time.now.strftime('%Y-%m-%d')}-#{title.to_url}.#{new_post_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  category = get_stdin("Enter category name to group your post in (leave blank for none): ")
+  tags = get_stdin("Enter tags to classify your post (comma separated): ")
+  puts "Creating new post: #{filename}"
+  open(filename, 'w') do |post|
+    post.puts "---"
+    post.puts "layout: post"
+    post.puts "title: \"#{title.gsub(/&/,'&amp;')}\""
+    post.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M:%S %z')}"
+    post.puts "category: [#{category}]"
+    post.puts "tags: [#{tags}]"
+    post.puts "image:"
+    post.puts "  feature: "
+    post.puts "  credit: "
+    post.puts "  creditlink: "
+    post.puts "comments: "
+    post.puts "share: "
+    post.puts "---"
+  end
+end
+
+# usage rake new_page
+desc "Create a new page"
+task :new_page, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin("Enter a title for your page: ")
+  end
+  filename = "#{title.to_url}.#{new_page_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  tags = get_stdin("Enter tags to classify your page (comma separated): ")
+  puts "Creating new page: #{filename}"
+  open(filename, 'w') do |page|
+    page.puts "---"
+    page.puts "layout: page"
+    page.puts "permalink: /#{title.to_url}/"
+    page.puts "title: \"#{title}\""
+    page.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M')}"
+    page.puts "tags: [#{tags}]"
+    page.puts "image:"
+    page.puts "  feature: "
+    page.puts "  credit: "
+    page.puts "  creditlink: "
+    page.puts "share: "
+    page.puts "---"
+  end
+end
+
+def get_stdin(message)
+  print message
+  STDIN.gets.chomp
+end
+
+def ask(message, valid_options)
+  if valid_options
+    answer = get_stdin("#{message} #{valid_options.to_s.gsub(/"/, '').gsub(/, /,'/')} ") while !valid_options.include?(answer)
+  else
+    answer = get_stdin(message)
+  end
+  answer
+end
